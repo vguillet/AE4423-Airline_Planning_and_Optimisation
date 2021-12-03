f1ef71d9c14293af4f818c993c2bc1f34429e3f8
hloopik <h.w.loopik@student.tudelft.nl>
2021-12-03 12:17:23 +0100

    example lect3

--- Problem_1/Usefull_functions.py
+++ Problem_1/Usefull_functions.py
@@ -19,3 +19,34 @@ def Distance(phi_i,phi_j,lambda_i,lambda_j,mode='deg'): ###in deg!
     term_2 = cos(phi_i)*cos(phi_j)*(sin( (lambda_i-lambda_j)/2 ))**2
     arc =  2 * arcsin(sqrt(term_1+term_2))
     return arc * R_E
+
+def pcol(input,c):
+    style = {'black' : '\033[30m',
+             'r'    : '\033[31m',
+             'g'  : '\033[32m',
+              'o' : '\033[33m',
+             'y' : '\033[93m',
+             'b'   : '\033[34m',
+              'lb'   : '\033[94m',
+             'p'  : '\033[35m',
+             'm'  : '\033[95m',
+             'c'   : '\033[36m',
+             'w' : '\033[97m',
+             'END'   : '\033[0m',
+             'B'   : '\033[1m',
+             'DIM' : '\033[2m',
+             'U' : '\033[4m',
+             'BLINK' : '\033[5m'}
+
+    string = ''
+    if c == 'RAINBOW':
+        rainbow = ['r','y','g','lb','b','p','m']
+        for i , s in enumerate(str(input)):
+            string+= style[rainbow[i%len(rainbow)]] + s
+    else:
+        c = c.split()
+        for i in c:
+            string += style[i]
+        string += f'{input}'
+
+    return string + style["END"]
